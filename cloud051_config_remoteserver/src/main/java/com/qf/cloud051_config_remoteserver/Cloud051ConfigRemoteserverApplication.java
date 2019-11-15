6a189510f5bafe7c0bccee9d4674dc4dcdc4b236
MarkGrady <1345022766@qq.com>
2019-11-15 20:51:54 +0800

    Initial commit

---
+++ cloud051_config_remoteserver/src/main/java/com/qf/cloud051_config_remoteserver/Cloud051ConfigRemoteserverApplication.java
@@ -0,0 +1,13 @@
+package com.qf.cloud051_config_remoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud051ConfigRemoteserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cloud051ConfigRemoteserverApplication.class, args);
+    }
+
+}
---
+++ cloud051_config_remoteserver/src/test/java/com/qf/cloud051_config_remoteserver/Cloud051ConfigRemoteserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.cloud051_config_remoteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud051ConfigRemoteserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
